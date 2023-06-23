0e331c214b21c1e0c546a9fa1f90452212f060c0
Sorady <125706196+Sorady@users.noreply.github.com>
2023-06-24 02:58:28 +0300

    Update5 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Sorady/alx-low_level_programming/edit/main/0x18-dynamic_libraries/101-make_me_win.sh
-export LD_PRELOAD=/tmp/libmask.so
+wget -P /tmp/ https://raw.github.com/Sorady/alx-low_level_programming/edit/main/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD =/tmp/nrandom.so
